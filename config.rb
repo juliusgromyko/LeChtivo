632801e4717f6bf305f7b8c9c40ce2ade19cdc56
Юлий Громыко <juliusgromko@gmail.com>
2014-01-23 16:39:55 +0200

    Initial commit

--- config.rb
+++ config.rb
@@ -17,9 +17,23 @@ javascripts_dir = "javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+css_dir = "css" # by Fire.app 
+sass_dir = "scss" # by Fire.app 
+images_dir = "img" # by Fire.app 
+javascripts_dir = "js" # by Fire.app 
+fireapp_coffeescripts_dir = "cs" # by Fire.app 
+fireapp_livescripts_dir = "ls" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :expanded # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = true # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>false} # by Fire.app 
